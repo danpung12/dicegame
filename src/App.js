79db43df517cd981e8152e8ffa88170b9db4c45a
aass6863 <aass6863@naver.com>
2024-03-18 00:02:27 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import Button from './Button';
+import Dice from './Dice';
+
+
+function random(n){
+  return Math.ceil(Math.random()*n);
+}
+
 
 function App() {
+  const [num, setNum] = useState(1);
+
+  function handleRollClick(){
+    const NextNum = random(6);
+    setNum(NextNum);
+  }
+  function handleClearClick(){
+    setNum(1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Button onClick={handleRollClick}>던지기</Button> 
+        <Button onClick={handleClearClick}>처음부터</Button> 
+      </div>
+      <Dice color="blue" num={num} />
     </div>
   );
 }
